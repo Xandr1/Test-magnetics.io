b040e54c02e4dfa33badc883a13e43f5624973d5
olek-kirsanov <olek.kirsanov@gmail.com>
2020-09-04 14:38:41 +0300

    3. prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,43 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    browser: true,
-    es6: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-console": [
-			"off"
-    ],
-    "no-var": [
-		"error"
-		],
-		"indent": [
-			1, "tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"no-loop-func": [
-			"error"
-		],
-		"block-spacing": [
-			"error",
-			"always"
-		],
-		"camelcase": [
-			"error"
-		],
-		"eqeqeq": [
-			"error",
-			"always"
-		],
-		"brace-style": [
-			"error",
-			"1tbs",
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es6: true
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-console': ['off'],
+		'no-var': ['error'],
+		indent: [1, 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		'no-loop-func': ['error'],
+		'block-spacing': ['error', 'always'],
+		camelcase: ['error'],
+		eqeqeq: ['error', 'always'],
+		'brace-style': [
+			'error',
+			'1tbs',
 			{
-				"allowSingleLine": true
+				allowSingleLine: true
 			}
 		],
-		"comma-style": [
-			"error",
-			"last"
-		],
-		"comma-spacing": [
-			"error",
+		'comma-style': ['error', 'last'],
+		'comma-spacing': [
+			'error',
 			{
-				"before": false,
-				"after": true
+				before: false,
+				after: true
 			}
 		],
-		"eol-last": [
-			"error"
-		],
-		"func-call-spacing": [
-			"error",
-			"never"
-		],
-  }
-};
+		'eol-last': ['error'],
+		'func-call-spacing': ['error', 'never']
+	}
+}
